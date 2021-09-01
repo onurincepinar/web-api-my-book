82b1ba826ca984f09b4919ea69953292c77210ac
Onur İncepınar <onurincepinar@gmail.com>
2021-09-01 13:44:17 +0300

    .Net 5.0 web api denemesi

---
+++ my-books/my-books/Data/Paging/PaginatedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Paging
+{
+    public class PaginatedList<T>:List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages{ get; private set; }
+
+        public PaginatedList(List<T> items,int count,int pageIndex,int pageSize)
+        {
+            PageIndex = pageIndex;
+            //Total pages dinamik sayı, yani item sayısı arttıkça toplam sayfa sayısı da artacak. 
+            //Örneğin sayfa büyüklüğü 5 ama bizde 8 tane item var. 5 tanesi ilk sayfada kalan 3 tanesi 2. sayfada gözükecek.
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return PageIndex > 1;
+            }
+        }
+        public bool HasNextPage
+        {
+            get
+            {
+                return PageIndex < TotalPages;
+            }
+
+        }
+
+        public static PaginatedList<T> Create(IQueryable<T> source,int pageIndex,int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageIndex).Take(pageSize).ToList();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
---
+++ my-books/my-books/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+                CreateHostBuilder(args).Build().Run();
+
+                //Log.Logger = new LoggerConfiguration()
+
+                //    //rollingInterval.Day her gün e ayrı log dosyası oluştur demek.
+                //    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+                //    .CreateLogger();
+                //CreateHostBuilder(args).Build().Run();
+
+            }
+            finally 
+            {
+
+                Log.CloseAndFlush();
+            }
+         
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
